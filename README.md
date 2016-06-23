4d63ee786f6f33f78fbe8769513561f03e8153b5
Katy Brimm <kbrimm@pdx.edu>
2016-06-23 16:25:00 -0700

    Usability updates
    Introduces global string constants for sensitive information.
    Enhanced input checking.
    Updated README to contain useful information.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # environmental-conditioner
+
 A random number generator to simulate the transmission of node data to a Cassandra database.
+
+## Use
+
+Edit file to set global variables for your Cassandra instance username, password, IP address, and target keyspace.
+
+If no arguments are supplied, or a non-integer argument is passed, environmental_conditioner will generate 9 nodes worth of data. A valid, positive integer argument will generate the specified number of nodes worth of data. Eg:
+
+    python environmental_conditioner.py
+
+results in 9 nodes, whereas
+
+    python environmental_conditioner.py 48
+
+results in 48 nodes.
+
+Barring a fatal error, environmental_conditioner will run indefinitely and can be terminated with an abort command (CTRL-C, etc).
+
+## License
+
+Copyright (c) 2016 Sapphire Becker, Katy Brimm, Scott Ewing, Matt Fraser, Kelly Ledford, Michael Limb, Steven Ngo, Eric Turley.
+
+This project is licensed under the MIT License. Please see the file LICENSE in this distribution for license terms.
--- environmental_conditioner.py
+++ environmental_conditioner.py
@@ -11,6 +11,12 @@ import time
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
+# Define global string variables
+USERNAME = ***
+PASSWORD = ***
+IP_ADDRESS = ***
+KEYSPACE = ***
+
 def get_conditions(vineId, hubId, nodeId):
     # Temp range: 15-85 degrees F
     temperature = random.randint(15, 85)
@@ -18,7 +24,7 @@ def get_conditions(vineId, hubId, nodeId):
     humidity = random.randint(25, 95)
     # Wetness range: 0-7000 k-Ohms
     leafwetness = random.randint(0, 7000) 
-    # datasent timestamp
+    # Datasent timestamp
     datasent = int(time.time()*1000)
     conditions = (vineId, hubId, nodeId, datasent, temperature,
                   humidity, leafwetness)
@@ -29,14 +35,14 @@ def generate_dataset(itemCount):
     for n in range(0, itemCount):
         # For the purposes of demonstration, each vineyard contains 18 nodes.
         v = int(n/18)
-        # For the purposes of demonstration, each hub supports 6 nodes.
-        h = int(n/6)
+        # For the purposes of demonstration, each hub supports 3 nodes.
+        h = int(n/3)
         dataset.append(get_conditions(v, h, n))
     return dataset
 
 def create_batch(dataset):
-    prologue = ('INSERT INTO plantalytics.environmental_data ( vineid, hubid, '
-                'nodeid, datasent, temperature, humidity, leafwetness, '
+    prologue = ('INSERT INTO ' + KEYSPACE + '.environmental_data ( vineid, '
+                'hubid, nodeid, datasent, temperature, humidity, leafwetness, '
                 'batchsent ) VALUES ( ')
     epilogue = str(int(time.time()*1000)) + ' );\n'
     batch = 'BEGIN BATCH\n'
@@ -56,9 +62,9 @@ def create_batch(dataset):
     
 def push_batch(batch):
     try:
-        auth = PlainTextAuthProvider(username=*username*, password=*password*)
-        cluster = Cluster(contact_points=[*ip*], auth_provider=auth)
-        session = cluster.connect(*keyspace*)
+        auth = PlainTextAuthProvider(username = USERNAME, password = PASSWORD)
+        cluster = Cluster(contact_points = [IP_ADDRESS], auth_provider = auth)
+        session = cluster.connect(KEYSPACE)
         session.execute(batch)
     except Exception as oops:
         print ('EXCEPTION:\n   ' + str(oops))
@@ -71,12 +77,13 @@ def bedtime():
     for t in range(0, 20):
         time.sleep(increment)
     
-def main():
-    # Parse the first supplied argument as an integer. If this succeeds, 
-    #   launch the loop with the speficied input.
+def main():    
+    # Parse the first supplied argument as a positive integer. If this 
+    #   succeeds, launch the loop with the speficied input.
     try:
         input = sys.argv[1]
         nodes = int(input)
+        assert nodes > 0
     # Otherwise, run with a 9 node sample
     except:
         nodes = 9    
